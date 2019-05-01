9ababa83b7fd84fa24c515c07947d522bbb98714
alagiesellu <alagiesellu@gmail.com>
2019-05-01 19:19:58 +0000

    Generated simple data set easy to learn. To test effectiveness of the LSTM model.

--- Functions.py
+++
@@ -1,8 +0,0 @@
-import json
-def store_vars(vars):
-    open('store/vars', 'w').write(str(vars))
-    print('variables stored: ', len(vars))
-
-def load_vars():
-    print(open('store/vars').read())
-    return json.load(open('store/vars').read())
